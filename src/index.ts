b8e9a278fbc7f66fc611060bd659f489ac9c9c2c
Alistair Smith <alistairsmith01@gmail.com>
2021-03-08 20:24:44 +0000

    feat: signale

--- src/index.ts
+++ src/index.ts
@@ -3,71 +3,71 @@ import "dotenv/config";
 import { Client, EmbedField, MessageEmbed } from "discord.js";
 import { commands } from "./commands";
 import { prisma } from "./prisma";
+import signale from "signale";
 
 const client = new Client();
 const prefix = "!";
 
 client.on("ready", () => {
-  console.log(`Ready as ${client.user?.tag}`);
+  signale.success(`Ready as ${client.user?.tag}`);
 });
 
 client.on("message", async (message) => {
-  if (message.content.startsWith(prefix)) {
-    const [caselessCommandName, ...args] = message.content.replace(prefix, "").split(" ");
-    const commandName = caselessCommandName.toLowerCase();
+  if (message.author.bot) return;
+  if (!message.content.startsWith(prefix)) return;
 
-    if (commandName && args[0]) {
-      const command = commands.get(args[0]);
+  const [caselessCommandName, ...args] = message.content.replace(prefix, "").split(" ");
+  const commandName = caselessCommandName.toLowerCase();
 
-      if (!command) {
-        return message.reply(`This command does not exist. Use ${prefix}help to see all commands`);
-      }
-
-      const embed = new MessageEmbed().addField("Description", command.description);
-
-      if (command.syntax) {
-        embed.addField("Syntax", command.syntax);
-      }
-
-      return message.reply(embed);
-    }
-
-    if (commandName === "help") {
-      const entries = [...commands.entries()];
-
-      const fields: EmbedField[] = entries.map((entry) => {
-        const [name, command] = entry;
-        return {
-          name: `${prefix}${name}`,
-          value: command.description,
-          inline: false,
-        };
-      });
-
-      const embed = new MessageEmbed().addFields(fields);
-
-      return message.reply(embed);
-    }
-
-    const command = commands.get(commandName);
+  if (commandName === "help" && args[0]) {
+    const command = commands.get(args[0]);
 
     if (!command) {
-      return message.reply("That command does not exist.");
+      return message.reply(`This command does not exist. Use ${prefix}help to see all commands`);
     }
 
-    const inhibitors = Array.isArray(command.inhibitors)
-      ? command.inhibitors
-      : [command.inhibitors];
+    const embed = new MessageEmbed().addField("Description", command.description);
 
-    try {
-      for (const inhibitor of inhibitors) {
-        inhibitor(message);
-      }
-
-      await command.run(message, args);
-    } catch (e) {
-      await message.reply(`⚠ ${e.message}`);
+    if (command.syntax) {
+      embed.addField("Syntax", command.syntax);
     }
+
+    return message.reply(embed);
+  }
+
+  if (commandName === "help") {
+    const entries = [...commands.entries()];
+
+    const fields: EmbedField[] = entries.map((entry) => {
+      const [name, command] = entry;
+      return {
+        name: `${prefix}${name}`,
+        value: command.description,
+        inline: false,
+      };
+    });
+
+    const embed = new MessageEmbed().addFields(fields);
+
+    return message.reply(embed);
+  }
+
+  const command = commands.get(commandName);
+
+  if (!command) {
+    return message.reply("That command does not exist.");
+  }
+
+  const inhibitors = Array.isArray(command.inhibitors) ? command.inhibitors : [command.inhibitors];
+
+  try {
+    for (const inhibitor of inhibitors) {
+      inhibitor(message);
+    }
+
+    await command.run(message, args);
+  } catch (e) {
+    await message.reply(`⚠ ${e.message}`);
   }
 });
 
